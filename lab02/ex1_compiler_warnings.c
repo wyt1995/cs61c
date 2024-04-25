668f21a8534fec005384e7c73c5e9a653d153a61
Yutong Wang <aeo9539@gmail.com>
2024-04-25 16:59:37 -0400

    lab2: gdb debugger

--- lab02/ex1_compiler_warnings.c
+++ lab02/ex1_compiler_warnings.c
@@ -7,13 +7,16 @@ struct Course {
     char *name;
 };
 
+
 struct Course *make_course(int id, char *name) {
     struct Course *new_course = malloc(sizeof(struct Course));
     new_course->id = id;
-    new_course->name = *name;
+    new_course->name = name;
 
-    return &new_course;
+    return new_course;
 }
+
+
 int main() {
     struct Course *cs161 = make_course(161, "Computer Security");
     printf("Welcome to CS%d: %s!\n", cs161->id, cs161->name);
