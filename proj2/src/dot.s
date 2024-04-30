bccb8ff5d7fd3a63cbcbbef5cf52e507714dcc8e
Yutong Wang <aeo9539@gmail.com>
2024-04-30 18:58:59 -0400

    [proj2] task 4

--- proj2/src/dot.s
+++ proj2/src/dot.s
@@ -18,27 +18,42 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
-    # Prologue
-
+    # Prologue: check args
+    li t0, 1
+    blt a2, t0, excp_number
+    blt a3, t0, excp_stride
+    blt a4, t0, excp_stride
+    
+    # dot product
+    li t0, 0
+    
+    # strides
+    slli t3, a3, 2
+    slli t4, a4, 2
 
 loop_start:
+    lw t1, 0(a0)
+    lw t2, 0(a1)
+    mul t1, t1, t2
+    add t0, t0, t1
 
-
-
-
-
-
-
-
-
-
-
+    addi a2, a2, -1
+    beq a2, x0, loop_end
+    
+    # Increment array addresses
+    add a0, a0, t3
+    add a1, a1, t4
+    j loop_start
 
 loop_end:
-
+    mv a0, t0
+    ret
 
     # Epilogue
-
-
-    jr ra
+excp_number:
+    li a0, 36
+    j exit
+    
+excp_stride:
+    li a0, 37
+    j exit
