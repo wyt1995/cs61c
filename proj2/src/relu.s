f5a625f295360486ab6bcd809728b9a5f6682295
Yutong Wang <aeo9539@gmail.com>
2024-04-30 18:58:40 -0400

    [proj2] task 2

--- proj2/src/relu.s
+++ proj2/src/relu.s
@@ -14,25 +14,38 @@
 # ==============================================================================
 relu:
     # Prologue
+    # Branch to exception if a1 is less than 1
+    addi t0, x0, 1
+    blt a1, t0, excp
 
-
+    # Load array index
+    li t1, 0
+    
 loop_start:
-
-
-
-
-
-
-
+    # Load array element
+    lw t2, 0(a0)
+    
+    # Branch if element is negative
+    blt t2, x0, loop_continue
+    
+    # Increment array address and index
+    addi a0, a0, 4
+    addi t1, t1, 1
+    
+    # End loop if no more element
+    beq t1, a1, loop_end
+    j loop_start
 
 loop_continue:
-
-
+    # Set negative element to 0
+    sw x0, 0(a0)
+    j loop_start
 
 loop_end:
-
+    jr ra
 
     # Epilogue
-
-
-    jr ra
+excp:
+    # Exit with error code 36
+    li a0, 36
+    j exit
