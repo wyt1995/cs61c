e3e4452e432ea70b7acf8008529eab4946dd05df
Yutong Wang <aeo9539@gmail.com>
2024-04-29 23:55:05 -0400

    [lab04] risc-v calling conventions

--- lab04/ex3.s
+++ lab04/ex3.s
@@ -42,7 +42,13 @@ ex3:
     mv t0 a0      # save a0 in t0
     addi a1 a1 -1 # decrement a1
 
+    addi sp sp -4
+    sw t0 0(sp)
+
     jal ex3       # call ex3(a0, a1-1)
+
+    lw t0 0(sp)
+    addi sp sp 4
 
     mul a0 a0 t0  # multiply ex3(a0, a1-1) by t0
                   # (which contains the value of a0)
