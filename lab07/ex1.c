88cd22fed5564db745bbea7686dc4fc003cbae5f
Yutong Wang <aeo9539@gmail.com>
2024-05-19 02:13:21 -0400

    [lab07] SIMD fixed wrong placement

--- lab07/ex1.c
+++ lab07/ex1.c
@@ -57,24 +57,9 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     __m128i sum_vect = _mm_setzero_si128();
     for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         unsigned int i = 0;
-        for (; i < NUM_ELEMS / 16 * 16; i += 16) {
+        for (; i < NUM_ELEMS / 4 * 4; i += 4) {
             __m128i temp = _mm_loadu_si128((__m128i *) (vals + i));
             __m128i comp = _mm_cmpgt_epi32(temp, _127);
-            temp = _mm_and_si128(temp, comp);
-            sum_vect = _mm_add_epi32(sum_vect, temp);
-
-            temp = _mm_loadu_si128((__m128i *) (vals + i + 4));
-            comp = _mm_cmpgt_epi32(temp, _127);
-            temp = _mm_and_si128(temp, comp);
-            sum_vect = _mm_add_epi32(sum_vect, temp);
-
-            temp = _mm_loadu_si128((__m128i *) (vals + i + 8));
-            comp = _mm_cmpgt_epi32(temp, _127);
-            temp = _mm_and_si128(temp, comp);
-            sum_vect = _mm_add_epi32(sum_vect, temp);
-
-            temp = _mm_loadu_si128((__m128i *) (vals + i + 12));
-            comp = _mm_cmpgt_epi32(temp, _127);
             temp = _mm_and_si128(temp, comp);
             sum_vect = _mm_add_epi32(sum_vect, temp);
         }
@@ -110,9 +95,24 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* Copy your sum_simd() implementation here, and unroll it */
         unsigned int i = 0;
-        for (; i < NUM_ELEMS / 4 * 4; i += 4) {
+        for (; i < NUM_ELEMS / 16 * 16; i += 16) {
             __m128i temp = _mm_loadu_si128((__m128i *) (vals + i));
             __m128i comp = _mm_cmpgt_epi32(temp, _127);
+            temp = _mm_and_si128(temp, comp);
+            sum_vect = _mm_add_epi32(sum_vect, temp);
+
+            temp = _mm_loadu_si128((__m128i *) (vals + i + 4));
+            comp = _mm_cmpgt_epi32(temp, _127);
+            temp = _mm_and_si128(temp, comp);
+            sum_vect = _mm_add_epi32(sum_vect, temp);
+
+            temp = _mm_loadu_si128((__m128i *) (vals + i + 8));
+            comp = _mm_cmpgt_epi32(temp, _127);
+            temp = _mm_and_si128(temp, comp);
+            sum_vect = _mm_add_epi32(sum_vect, temp);
+
+            temp = _mm_loadu_si128((__m128i *) (vals + i + 12));
+            comp = _mm_cmpgt_epi32(temp, _127);
             temp = _mm_and_si128(temp, comp);
             sum_vect = _mm_add_epi32(sum_vect, temp);
         }
@@ -124,8 +124,13 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
         }
 
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for (; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
-
+    
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
     clock_t end = clock();
     printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
